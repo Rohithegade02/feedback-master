dfc828335415dc7a4a43e36fce9daaf92a33efe2
Rohithegade02 <rohithegade8@gmail.com>
2023-03-04 22:43:49 +0530

    first commit

---
+++ backend/controllers/Chat.js
@@ -0,0 +1,36 @@
+const Chat = require('../models/Chat')
+const Complaint = require('../models/Complaint')
+
+exports.chat_registration = async(req, res) => {
+    try{
+        const { 
+            text,
+            sender_id,
+            complaint_id
+        } = req.body
+
+        const chat =  new Chat({
+            text,
+            sender_id,
+            complaint_id
+        })
+
+        await chat.save()
+
+        const fullChat = await Chat.findOne({ _id: chat._id }).populate('sender_id' , `name role`).sort({date: 1})
+        res.send({message: 'Chat Saved', chat: fullChat})
+    } catch(e) {
+        res.status(500).json(e)
+    }
+}
+
+exports.get_complaints_chat = async(req, res) => {
+    try{
+        const chat = await Chat.find({ complaint_id: req.params.id }).populate('sender_id' , `name role`).sort({date: 1})
+        const complaint = await Complaint.findById(req.params.id).populate('student_id' , `name`).select('subject complaint_details anonymity resolved date')
+
+        res.send({chat, complaint})
+    } catch(e) {
+        res.status(500).json(e)
+    }
+}
---
+++ frontend/src/pages/ComplaintsPage/TeachersComplaintPage/TeachersComplaintPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ComplaintSection from '../../../components/ComplaintsSection/ComplaintsSection'
+import './TeachersComplaintPage.scss'
+import { decodeSessionStorage } from '../../../helpers/helpers';
+
+const TeachersComplaintPage = () => {
+    return (
+        <ComplaintSection 
+            APIurl={`${process.env.REACT_APP_API_URL}/complaint/get/all/school/${decodeSessionStorage().school_id}`}
+            ListItemLink='/complaints/'
+        />
+    )
+}
+
+export default TeachersComplaintPage
